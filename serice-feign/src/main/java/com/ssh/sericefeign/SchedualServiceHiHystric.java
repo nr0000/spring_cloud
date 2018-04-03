9499c933c23bea07518c825a0b015908bb8dde2c
nr0000 <648280276@qq.com>
2018-04-03 14:36:50 +0800

    Initial commit

---
+++ serice-feign/src/main/java/com/ssh/sericefeign/SchedualServiceHiHystric.java
@@ -0,0 +1,11 @@
+package com.ssh.sericefeign;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
---
+++ server/src/main/java/com/ssh/server/ServerApplication.java
@@ -0,0 +1,19 @@
+package com.ssh.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerApplication {
+
+	/**
+	 * spring cloud学习代码
+	 * https://blog.csdn.net/forezp/article/details/70148833
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
