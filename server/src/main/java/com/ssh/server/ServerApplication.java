3b9f79499d8164992256c1a5b3248f54e53f2d50
nr0000 <648280276@qq.com>
2018-04-12 18:02:25 +0800

    。。

--- server/src/main/java/com/ssh/server/ServerApplication.java
+++ server/src/main/java/com/ssh/server/ServerApplication.java
@@ -11,6 +11,8 @@ public class ServerApplication {
 	/**
 	 * spring cloud学习代码
 	 * https://blog.csdn.net/forezp/article/details/70148833
+	 *
+	 * eureka集群学习：https://blog.csdn.net/tianyaleixiaowu/article/details/78184793
 	 * @param args
 	 */
 	public static void main(String[] args) {
